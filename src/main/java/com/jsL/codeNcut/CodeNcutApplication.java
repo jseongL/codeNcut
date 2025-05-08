71d0e6b5cfdea2f23dad5fe18cfbc050afeb122e
DESKTOP-K9PSSTB\user <imi90655@naver.com>
2025-05-08 19:23:56 +0900

    [임지성] -aws배포용

--- src/main/java/com/jsL/codeNcut/CodeNcutApplication.java
+++ src/main/java/com/jsL/codeNcut/CodeNcutApplication.java
@@ -2,9 +2,10 @@ package com.jsL.codeNcut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CodeNcutApplication {
+public class CodeNcutApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeNcutApplication.class, args);
