41d744e0eb3728e874417ef9c2bb8d329f5be3d3
jsL <imi90655@naver.com>
2025-05-19 17:10:13 +0900

    [임지성] -학원용 로컬호스트 코드로 수정, Elastic Search 의존성 주석처리

--- src/main/java/com/jsL/codeNcut/buy/repository/BuySearchRepository.java
+++ src/main/java/com/jsL/codeNcut/buy/repository/BuySearchRepository.java
@@ -1,18 +1,18 @@
-package com.jsL.codeNcut.buy.repository;
-import java.util.List;
-
-import org.springframework.data.elasticsearch.annotations.Query;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.jsL.codeNcut.buy.domain.BuyDocument;
-
-public interface BuySearchRepository extends ElasticsearchRepository<BuyDocument, Integer> {
-
-    @Query("{\"bool\": {\"should\": [" +
-            "{\"match_phrase\": {\"model\": \"?0\"}}," +
-            "{\"match_phrase\": {\"description\": \"?0\"}}" +
-            "]}}")
-    List<BuyDocument> search(String text);
-
-    List<BuyDocument> findByUserId(int userId);
-}
+//package com.jsL.codeNcut.buy.repository;
+//import java.util.List;
+//
+//import org.springframework.data.elasticsearch.annotations.Query;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//
+//import com.jsL.codeNcut.buy.domain.BuyDocument;
+//
+//public interface BuySearchRepository extends ElasticsearchRepository<BuyDocument, Integer> {
+//
+//    @Query("{\"bool\": {\"should\": [" +
+//            "{\"match_phrase\": {\"model\": \"?0\"}}," +
+//            "{\"match_phrase\": {\"description\": \"?0\"}}" +
+//            "]}}")
+//    List<BuyDocument> search(String text);
+//
+//    List<BuyDocument> findByUserId(int userId);
+//}
